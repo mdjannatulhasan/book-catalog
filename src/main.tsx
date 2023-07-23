fa89b85b1e8f3a990c5f52dcef687e3dadc6ebc1
Shah Muhammad Jannatul Hasan <shahmdjhm@gmail.com>
2023-07-24 02:08:30 +0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { RouterProvider } from 'react-router-dom';
 import routes from './routes/routes.tsx';
 import { Provider } from 'react-redux';
 import store from './redux/store.ts';
+import React from 'react';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
