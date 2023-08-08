61d2a678037e23860cffe6504315546e672dd558
Shah Muhammad Jannatul Hasan <shahmdjhm@gmail.com>
2023-08-08 21:09:47 +0600

    Update apiSlice.ts

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -2,10 +2,10 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
     reducerPath: 'api',
-    // baseQuery: fetchBaseQuery({
-    //     baseUrl: 'https://book-catalog-server-smoky.vercel.app/api/v1',
-    // }),
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5001/api/v1' }),
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://book-catalog-server-smoky.vercel.app/api/v1',
+    }),
+    // baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5001/api/v1' }),
     tagTypes: ['books', 'book', 'reviews', 'wishlist'],
     endpoints: () => ({}),
 });
